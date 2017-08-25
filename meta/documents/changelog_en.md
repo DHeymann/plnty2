c29ffd1222cbb54e5a4a63bcf74a22c7b9225f7d
DHeymann <heymann@tubach-partner.de>
2017-08-25 11:09:15 +0200

    Schuhe Reset

---
+++ meta/documents/changelog_en.md
@@ -0,0 +1,47 @@
+# Release Notes for Elastic Export Shopping24.de
+
+## v1.0.8 (2017-08-09)
+
+### Fixed
+ - An issue was fixed which caused the images not to be ordered by the position.
+
+## v1.0.7 (2017-07-18)
+
+### Changed
+ - The plugin Elastic Export is now required to use the plugin format.
+
+## v1.0.6 (2017-05-24)
+
+### Changed
+- The plugin Elastic Export is now required to use the plugin format Shopping24DE.
+
+## v1.0.5 (2017-04-21)
+
+### Fixed
+- We changed the delimiter from space to tab.
+
+## v1.0.4 (2017-03-22)
+
+### Fixed
+- We now use a different value to get the image URLs for plugins working with elastic search.
+
+## v1.0.3 (2017-03-13)
+
+### Added
+- Added marketplace name.
+
+### Changed
+- Changed plugin icons.
+
+## v1.0.2 (2017-03-03)
+- Adjustment for the ResultField, so the imageMutator does not affect the image outcome anymore if the referrer "ALL" is set
+
+## v1.0.1 (2017-03-01)
+
+# Changed
+- Updated screenshot.
+
+## v1.0.0 (2017-02-20)
+ 
+### Added
+- Added initial plugin files
---
+++ src/ResultField/Shopping24DE.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace ElasticExportShopping24DE\ResultField;
+
+use Plenty\Modules\DataExchange\Contracts\ResultFields;
+use Plenty\Modules\DataExchange\Models\FormatSetting;
+use Plenty\Modules\Helper\Services\ArrayHelper;
+use Plenty\Modules\Item\Search\Mutators\ImageMutator;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\BuiltIn\LanguageMutator;
+use Plenty\Modules\Item\Search\Mutators\SkuMutator;
+use Plenty\Modules\Item\Search\Mutators\DefaultCategoryMutator;
+
+class Shopping24DE extends ResultFields
+{
+    /*
+     * @var ArrayHelper
+     */
+    private $arrayHelper;
+
+    /**
+     * Shopping24DE constructor.
+     * @param ArrayHelper $arrayHelper
+     */
+    public function __construct(ArrayHelper $arrayHelper)
+    {
+        $this->arrayHelper = $arrayHelper;
+    }
+
+    public function generateResultFields(array $formatSettings = []):array
+    {
+        $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
+
+        $reference = $settings->get('referrerId') ? $settings->get('referrerId') : -1;
+
+        $itemDescriptionFields = ['texts.urlPath'];
+        $itemDescriptionFields[] = 'texts.keywords';
+
+        switch($settings->get('nameId'))
+        {
+            case 1:
+                $itemDescriptionFields[] = 'texts.name1';
+                break;
+            case 2:
+                $itemDescriptionFields[] = 'texts.name2';
+                break;
+            case 3:
+                $itemDescriptionFields[] = 'texts.name3';
+                break;
+            default:
+                $itemDescriptionFields[] = 'texts.name1';
+                break;
+        }
+
+        if($settings->get('descriptionType') == 'itemShortDescription'
+            || $settings->get('previewTextType') == 'itemShortDescription')
+        {
+            $itemDescriptionFields[] = 'texts.shortDescription';
+        }
+
+        if($settings->get('descriptionType') == 'itemDescription'
+            || $settings->get('descriptionType') == 'itemDescriptionAndTechnicalData'
+            || $settings->get('previewTextType') == 'itemDescription'
+            || $settings->get('previewTextType') == 'itemDescriptionAndTechnicalData')
+        {
+            $itemDescriptionFields[] = 'texts.description';
+        }
+        $itemDescriptionFields[] = 'texts.technicalData';
+
+        //Mutator
+        /**
+         * @var ImageMutator $imageMutator
+         */
+        $imageMutator = pluginApp(ImageMutator::class);
+        $imageMutator->addMarket($reference);
+        /**
+         * @var LanguageMutator $languageMutator
+         */
+        $languageMutator = pluginApp(LanguageMutator::class, [[$settings->get('lang')]]);
+        /**
+         * @var SkuMutator $skuMutator
+         */
+        $skuMutator = pluginApp(SkuMutator::class);
+        $skuMutator->setMarket($reference);
+        /**
+         * @var DefaultCategoryMutator $defaultCategoryMutator
+         */
+        $defaultCategoryMutator = pluginApp(DefaultCategoryMutator::class);
+        if($defaultCategoryMutator instanceof DefaultCategoryMutator)
+        {
+            $defaultCategoryMutator->setPlentyId($settings->get('plentyId'));
+        }
+
+        $fields = [
+            [
+                //item
+                'item.id',
+                'item.manufacturer.id',
+
+                //variation
+                'id',
+                'variation.availability.id',
+                'variation.stockLimitation',
+                'variation.vatId',
+                'variation.model',
+
+                //images
+                'images.item.urlMiddle',
+                'images.item.urlPreview',
+                'images.item.urlSecondPreview',
+                'images.item.url',
+                'images.item.path',
+                'images.item.position',
+
+                'images.variation.urlMiddle',
+                'images.variation.urlPreview',
+                'images.variation.urlSecondPreview',
+                'images.variation.url',
+                'images.variation.path',
+                'images.variation.position',
+
+                //unit
+                'unit.content',
+                'unit.id',
+
+                //defaultCategories
+                'defaultCategories.id',
+
+                //barcodes
+                'barcodes.code',
+                'barcodes.type',
+
+                //attributes
+                'attributes.attributeValueSetId',
+            ],
+
+            [
+                $languageMutator,
+                $skuMutator,
+                $defaultCategoryMutator
+            ],
+        ];
+
+        if($reference != -1)
+        {
+            $fields[1][] = $imageMutator;
+        }
+
+        foreach($itemDescriptionFields as $itemDescriptionField)
+        {
+            $fields[0][] = $itemDescriptionField;
+        }
+
+        return $fields;
+    }
+}
